3e91ea86e7a9bfc10e799e823e08089ec50242d3
frybitsinc <frybitsofficial@gmail.com>
2016-11-24 03:16:24 +0900

    join db insert to users table success

--- meadowlark2.js
+++ meadowlark2.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 //sql connect
 /*
 const pg = require('pg');
@@ -34,6 +37,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 //app.post method : for insert query
 app.post('/join', function(req, res, next){
+	console.log(req);
 	var userid = req.body.userid;
 	var password = req.body.password;
 	var nickname = req.body.nickname;  
@@ -41,6 +45,55 @@ app.post('/join', function(req, res, next){
 	var gender = req.body.gender;
 	var joindate = Date.now();
 //query
+	var mysql = require('mysql');
+	var connection = mysql.createConnection({
+		host : '192.168.123.105',
+		user     : 'root',
+		password: 'masickdang'
+	});
+/*
+connection.on('connect', function(err) {  
+	// If no error, then good to proceed.  
+        console.log("Connected");
+	if(err) return console.error(err);
+	executeStatement();
+});
+*/
+connection.connect(function(err) {
+	// CONNECT
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+	// USE DATABASE
+	connection.query('USE MASICKDANG;', function(err, rows) {
+		if (err) {
+			console.error('db use error: ' + err.stack);
+			return;
+		}
+		console.log('db use successfully');
+	});
+	// INSERT
+	// var query = connection.query('INSERT INTO users (userID, passwd, nickName, emailAddress, sex, birthYear, birthMonth, birthDay) VALUES($1, $2, $3, $4, $5, $6, $7, $8)', [userid, password, nickname, email, gender, '1993', '6', '18'], function (err, result) {
+	console.log('id: ' + userid + '\npassword: ' + password + '\nnickname: ' + nickname + '\nemail: ' + email + '\ngender: ' + gender + '\njoindate: ' + joindate)
+	var query = connection.query('INSERT INTO users SET ?', {'userID':userid, 'passwd':password, 'nickName': nickname, 'emailAddress':email, 'sex':gender, 'birthYear': '1993', 'birthMonth':'6', 'birthDay': '18'}, function (err, result) {
+		if (err) {
+			console.error(err);
+			return next(err);
+		}
+		var query=connection.query('select * from users', function (err, result) {
+//done(); //this done callback signals the pg driver that the connection can be closed or returned to the connection pool
+			if (err) {
+        			// pass the error to the express error handler
+				console.error(err);
+				return next(err);
+			}
+			console.log(query);
+			res.send(200, 'success');
+		});
+	});
+//////////////////////////////////////////////////////////////////////
 /*
  pg.connect(conString, function (err, client, done) {
     if (err) {
@@ -61,6 +114,7 @@ done(); //this done callback signals the pg driver that the connection can be cl
   });
 */
 
+});
 });
 app.post('/search', function(req, res){
 	var menu = {
@@ -85,13 +139,37 @@ app.get('/join', function(req, res){
 
 app.get('/search', function(req, res){
 	res.render('search');
-var connection = new Connection(config);  
+// var connection = new Connection(config);  
+	var mysql = require('mysql');
+	var connection = mysql.createConnection({
+  host : '192.168.123.105',
+  user     : 'root',
+  password: 'masickdang'
+});
+/*
 connection.on('connect', function(err) {  
 	// If no error, then good to proceed.  
         console.log("Connected");
 	if(err) return console.error(err);
 	executeStatement();
 });
+*/
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
+connection.query('use MASICKDANG;', function(err, rows) {
+  if (err) {
+    console.error('db use error: ' + err.stack);
+    return;
+  }
+  console.log('db use successfully');
+});
+
 var Request = require('tedious').Request;  
     var TYPES = require('tedious').TYPES;  
   
